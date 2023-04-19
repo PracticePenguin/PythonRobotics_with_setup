125ffb2b30f3d9daa58122cec92bdc62c7a5d7cd
PracticePenguin <sheng.yang@tum.de>
2023-04-19 15:39:01 +0200

    copied python folder with setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='PythonRobotics',
+      version='1.0',
+      description='PythonRobotics modules',
+      author='Atsushi Sakai',
+      url='https://github.com/AtsushiSakai/PythonRobotics.git',
+      packages=['PathPlanning'],
+     )
