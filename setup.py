aca967b1f59cedfbcee6a5858c6e222425fd89c3
PracticePenguin <sheng.yang@tum.de>
2023-04-19 20:20:55 +0200

    include

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='PythonRobotics',
       description='PythonRobotics modules',
       author='Atsushi Sakai',
       url='https://github.com/AtsushiSakai/PythonRobotics.git',
-      packages=find_namespace_packages(exclude=["tests"]),
+      packages=find_namespace_packages(include=["PathPlanning"]),
      )
