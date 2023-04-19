ec52c6812abd151b66026ef607849034bdf63e7d
PracticePenguin <sheng.yang@tum.de>
2023-04-19 21:39:20 +0200

    added requires

--- setup.py
+++ setup.py
@@ -7,4 +7,13 @@ setup(name='PythonRobotics',
       author='Atsushi Sakai',
       url='https://github.com/AtsushiSakai/PythonRobotics.git',
       packages=find_namespace_packages(include=["PathPlanning", "PathPlanning.*"]),
+      install_requires=[
+            "numpy == 1.24.2",
+            "scipy == 1.10.1",
+            "matplotlib == 3.7.1",
+            "cvxpy == 1.3.1",
+            "pytest == 7.3.0",
+            "pytest-xdist == 3.2.1",
+            "mypy == 1.2.0",
+            "ruff == 0.0.261"],
      )
